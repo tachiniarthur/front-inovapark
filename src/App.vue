f17ac1e6652ae4f6a66b38d3d8b32e1554276190
tachini <tachiniarthur@gmail.com>
2025-03-27 20:04:40 -0300

    Create fetch user test in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,49 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(async () => {
+  try {
+    createUser()
+    const response = await fetch('http://localhost:8080/users')
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.statusText}`)
+    }
+    const data = await response.json()
+    console.log('Dados da API:', data)
+  } catch (error) {
+    console.error('Erro ao buscar dados da API:', error)
+  }
+})
+
+async function createUser() {
+  const matricula = '123455'
+  const cpf = '123456789002'
+  const senha = '12345678900'
+  const nomeCompleto = '123456789003'
+  const email = '123456789005'
+
+  const response = await fetch('http://localhost:8080/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      matricula: matricula,
+      cpf: cpf,
+      senha: senha,
+      nome: nomeCompleto,
+      email: email,
+    }),
+  })
+
+  if (!response.ok) {
+    throw new Error(`Erro na requisição: ${response.statusText}`)
+  }
+  const data = await response.json()
+  console.log('Dados do usuário criado:', data)
+  return data
+}
+</script>
 
 <template>
   <main>
