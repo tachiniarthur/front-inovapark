8b9a5cae5e7e24c7a27e95579e7ac5ae6c6e7f15
tachini <tachiniarthur@gmail.com>
2025-03-28 20:19:25 -0300

    Create login view

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,52 @@
 <script setup>
-import { onMounted } from 'vue'
+// import { onMounted } from 'vue'
 
-onMounted(async () => {
-  try {
-    createUser()
-    const response = await fetch('http://localhost:8080/users')
-    if (!response.ok) {
-      throw new Error(`Erro na requisição: ${response.statusText}`)
-    }
-    const data = await response.json()
-    console.log('Dados da API:', data)
-  } catch (error) {
-    console.error('Erro ao buscar dados da API:', error)
-  }
-})
+// onMounted(async () => {
+//   try {
+//     createUser()
+//     const response = await fetch('http://localhost:8080/users')
+//     if (!response.ok) {
+//       throw new Error(`Erro na requisição: ${response.statusText}`)
+//     }
+//     const data = await response.json()
+//     console.log('Dados da API:', data)
+//   } catch (error) {
+//     console.error('Erro ao buscar dados da API:', error)
+//   }
+// })
 
-async function createUser() {
-  const matricula = '123455'
-  const cpf = '123456789002'
-  const senha = '12345678900'
-  const nomeCompleto = '123456789003'
-  const email = '123456789005'
+// async function createUser() {
+//   const matricula = '123455'
+//   const cpf = '123456789002'
+//   const senha = '12345678900'
+//   const nomeCompleto = '123456789003'
+//   const email = '123456789005'
 
-  const response = await fetch('http://localhost:8080/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      matricula: matricula,
-      cpf: cpf,
-      senha: senha,
-      nome: nomeCompleto,
-      email: email,
-    }),
-  })
+//   const response = await fetch('http://localhost:8080/users', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       matricula: matricula,
+//       cpf: cpf,
+//       senha: senha,
+//       nome: nomeCompleto,
+//       email: email,
+//     }),
+//   })
 
-  if (!response.ok) {
-    throw new Error(`Erro na requisição: ${response.statusText}`)
-  }
-  const data = await response.json()
-  console.log('Dados do usuário criado:', data)
-  return data
-}
+//   if (!response.ok) {
+//     throw new Error(`Erro na requisição: ${response.statusText}`)
+//   }
+//   const data = await response.json()
+//   console.log('Dados do usuário criado:', data)
+//   return data
+// }
 </script>
 
 <template>
-  <main>
-    <p class="text-3xl underline">FRONT INOVAPARK</p>
-  </main>
+  <router-view />
 </template>
 
 <style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+  <main class="h-screen w-full flex items-center justify-center bg-gray-50">
+    <section class="w-full max-w-sm p-6 bg-white rounded-xl shadow-md">
+      <img :src="'logo.png'" alt="Logo" class="w-48 mx-auto h-auto mb-4" />
+      <h1 class="text-2xl font-bold text-center mb-4">Login</h1>
+      <form method="POST">
+        <div class="mb-4">
+          <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+          <input
+            type="email"
+            id="email"
+            class="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+            required
+          />
+        </div>
+        <div class="mb-4">
+          <label for="password" class="block text-sm font-medium text-gray-700">Senha</label>
+          <input
+            type="password"
+            id="password"
+            class="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+            required
+          />
+          <a href="/forgot-password" class="text-sm font-light text-blue-600 hover:underline"
+            >Esqueceu sua senha?</a
+          >
+        </div>
+        <button
+          type="submit"
+          class="w-full py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 hover:scale-105 transition duration-200 cursor-pointer"
+        >
+          Entrar
+        </button>
+      </form>
+      <p class="mt-4 text-sm text-center">
+        Não tem uma conta? <a href="/register" class="text-blue-600 hover:underline">Registrar</a>
+      </p>
+      <p class="mt-4 text-sm text-center">
+        Voltar para a <a href="/" class="text-blue-600 hover:underline">página inicial</a>
+      </p>
+    </section>
+  </main>
+</template>
+
+<script setup></script>
